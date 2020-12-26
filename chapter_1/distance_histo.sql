d64d9a06325fc4a645c1e05a165937b2c098394e
Anthony Virtuoso <avirtuos@gmail.com>
2020-12-26 13:31:36 -0500

    Update distance_histo.sql

--- chapter_1/distance_histo.sql
+++ chapter_1/distance_histo.sql
@@ -1,5 +1,10 @@
-SELECT trip_distance, number_of_rides
+SELECT trip_distance, number_rides
 FROM 
-    (SELECT numeric_histogram(10,trip_distance)
-    FROM packt_serverless_analytics.nyc_taxi ) AS x (ride_histogram)
-CROSS JOIN UNNEST(ride_histogram) AS t (trip_distance , number_of_rides);
+    (SELECT numeric_histogram(5,trip_distance)
+       FROM packt_serverless_analytics.nyc_taxi 
+       WHERE date_diff('minute',
+ 		date_parse(tpep_pickup_datetime,'%Y-%m-%d %H:%i:%s'),
+ 		date_parse(tpep_dropoff_datetime, '%Y-%m-%d %H:%i:%s')
+         ) <= 6.328061
+    ) AS x (ride_histogram)
+CROSS JOIN UNNEST(ride_histogram) AS t (trip_distance , number_rides);
